323fdef04224a3630d8533c90e5793b02d742136
sihaocao <41862661+sihaocao@users.noreply.github.com>
2019-09-02 00:23:15 -0400

    Add files via upload

---
+++ database.js
@@ -0,0 +1,132 @@
+const questionBank = [
+    {
+        question: "What is the national dish of Australia?",
+        answers: [
+            "Chicken Parmigiana",
+            "Meat Pie",
+            "Lamington",
+            "Pavlova"
+        ],
+        correctAnswer: "Meat Pie",
+        description: "https://en.wikipedia.org/wiki/Meat_pie",
+        icon: "https://i.imgur.com/Qhy3NCS.jpg",
+        alt: "Picture of Meat Pie"
+    },
+    {
+        question: "What is the national dish of Brazil?",
+        answers: [
+            "Coxinha",
+            "Pastel",
+            "Feijoada",
+            "Farofa"
+        ],
+        correctAnswer: "Feijoada",
+        description: "https://en.wikipedia.org/wiki/Feijoada",
+        icon: "https://i.imgur.com/AXeXxWT.jpg",
+        alt: "Picture of Feijoada"
+    },
+    {
+        question: "What is the national dish of Canada?",
+        answers: [
+            "Pea Soup",
+            "Poutine",
+            "Bagel",
+            "Pemmican"
+        ],
+        correctAnswer: "Poutine",
+        description: "https://en.wikipedia.org/wiki/Poutine",
+        icon: "https://i.imgur.com/PI6cbg0.jpg",
+        alt: "Picture of Poutine"
+    },
+    {
+        question: "What is the national dish of China?",
+        answers: [
+            "Kung Pao Chicken",
+            "Mapo Tofu",
+            "Dumpling",
+            "Peking Duck"
+        ],
+        correctAnswer: "Peking Duck",
+        description: "https://en.wikipedia.org/wiki/Peking_duck",
+        icon: "https://i.imgur.com/Wrs6RsR.jpg",
+        alt: "Picture of Peking Duck"
+    },
+    {
+        question: "What is the national dish of England?",
+        answers: [
+            "Fish and Chips",
+            "Bangers and Mash",
+            "Shepherd's Pie",
+            "Toad In The Hole"
+        ],
+        correctAnswer: "Fish and Chips",
+        description: "https://en.wikipedia.org/wiki/Fish_and_chips",
+        icon: "https://i.imgur.com/rAUnRK0.jpg",
+        alt: "Picture of Chicken Tikka Masala"
+    },
+    {
+        question: "What is the national dish of France?",
+        answers: [
+            "Cassoulet",
+            "Crepe",
+            "Beef Bourguignon",
+            "Flamiche"
+        ],
+        correctAnswer: "Crepe",
+        description: "https://en.wikipedia.org/wiki/Cr%C3%AApe",
+        icon: "https://i.imgur.com/43HFVht.jpg",
+        alt: "Picture of Crepe"
+    },
+    {
+        question: "What is the national dish of Germany?",
+        answers: [
+            "Rinderroulade",
+            "Bratwurst",
+            "Kartoffelpuffer",
+            "Sauerbraten"
+        ],
+        correctAnswer: "Sauerbraten",
+        description: "https://en.wikipedia.org/wiki/Sauerbraten",
+        icon: "https://i.imgur.com/tbLzppU.jpg",
+        alt: "Picture of Sauerbraten"
+    },
+    {
+        question: "What is the national dish of Greece?",
+        answers: [
+            "Dolmades",
+            "Moussaka",
+            "Baklava",
+            "Spanikopita"
+        ],
+        correctAnswer: "Moussaka",
+        description: "https://en.wikipedia.org/wiki/Moussaka",
+        icon: "https://i.imgur.com/tj9csTQ.jpg",
+        alt: "Picture of Moussaka"
+    },
+    {
+        question: "What is the national dish of India?",
+        answers: [
+            "Biryani",
+            "Aloo Gobi",
+            "Samosa",
+            "Matar Paneer"
+        ],
+        correctAnswer: "Biryani",
+        description: "https://en.wikipedia.org/wiki/Biryani",
+        icon: "https://i.imgur.com/RwnyUcQ.jpg",
+        alt: "Picture of Biryani"
+    },
+    {
+        question: "What is the national dish of USA?",
+        answers: [
+            "Jambalaya",
+            "Apple Pie",
+            "Hamburger",
+            "Deep Dish Pizza"
+        ],
+        correctAnswer: "Hamburger",
+        description: "https://en.wikipedia.org/wiki/Hamburger",
+        icon: "https://i.imgur.com/WMtPvll.jpg",
+        alt: "Picture of Hamburger"
+    },
+];
